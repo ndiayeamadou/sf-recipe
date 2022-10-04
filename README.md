fdf74e7d6faed9a419cd067c6011689c6036de5c
Amadu <informaticienprogrammeur@gmail.com>
2022-10-04 15:24:07 +0000

    generate & save fake data using fixtures & fakephp

---
+++ README.md
@@ -0,0 +1,24 @@
+```bash
+php bin/console doctrine:database:create OR
+php bin/console d:d:c
+```
+```bash
+php bin/console make:entity
+```
+```bash
+php bin/console make:migration
+```
+# migrate all migrations into DB
+```bash
+php bin/console doctrine:migrations:migrate
+```
+
+* in the development env :
+** Using fixtures to load fake data & also fakephp
+composer require --dev orm-fixtures
+composer require fakerphp/faker --dev
+
+* load these fake data in DB
+```bash
+php bin/console doctrine:fixtures:load
+```
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Ingredient;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+
+class AppFixtures extends Fixture
+{
+    /** using fakerphp */
+    private Generator $faker;
+    public function __construct() {
+        $this->faker = Factory::create();
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        //$ingredient->setName('Ingredient # 1')->setPrice('15.2');
+        for($i=0; $i<=10; $i++) {
+            $ingredient = new Ingredient();
+            //$ingredient->setName('Ingredient # '.$i)
+            //            ->setPrice(rand(0,200));
+            /** using fakerphp */
+            $ingredient->setName($this->faker->word())->setPrice(rand(0,200));
+            $manager->persist($ingredient);
+        }
+
+        $manager->flush();
+
+    }
+}
