99fe97381e8006524c10f19e48766bd8f00becd6
Amadu <informaticienprogrammeur@gmail.com>
2022-11-24 18:29:38 +0000

    send mail :e-mail:

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,9 +5,12 @@ namespace App\Controller;
 use App\Form\ContactFormType;
 use App\Entity\Contact;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
@@ -21,8 +24,11 @@ class ContactController extends AbstractController
     }
 
     #[Route('/contact', name: 'recipe_contact')]
-    public function createContact(ContactFormType $contactType, Request $request, EntityManagerInterface $emanager) {
-        /** récupérer les données de l'user s'il est connecté */
+    public function createContact(
+        ContactFormType $contactType, Request $request, EntityManagerInterface $emanager,
+        MailerInterface $mailer
+    ) {
+        /** récupérer les données de l'user s'il est connecté pour pré-remplir le formulaire */
         $contact = new Contact();
         if($this->getUser()) {
             $contact->setFullName($this->getUser()->getFullName())->setEmail($this->getUser()->getEmail());
@@ -33,6 +39,18 @@ class ContactController extends AbstractController
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()) {
             //dd($form->getData());
+            //dd($form->getData()->getFullName());
+            //dd($contact->getEmail());
+
+            /** sending mail */
+            $email = (new TemplatedEmail())
+                        ->from($contact->getEmail())->to('admin@gesrecipe.com')
+                        ->subject($contact->getSubject())
+                        ->htmlTemplate('emails/contact.html.twig')
+                        ->context(['contact' => $contact]);
+            $mailer->send($email);
+            /** end sending mail */
+
             $emanager->persist($form->getData());
             $emanager->flush();
             $this->addFlash('success', 'Votre message a bien été envoyé !');
